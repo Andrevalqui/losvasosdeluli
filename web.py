ae822e8b0bc782bb58b4b48dfe7ff37024bfdd6c
Andrevalqui <luiggi.valqui@tecsup.edu.pe>
2026-01-11 12:42:04 -0500

    Update web.py

--- web.py
+++ web.py
@@ -15,7 +15,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# 2. DATOS (Mantenemos tus links originales)
+# 2. DATOS
 INFO = {
     "nombre": "Los Vasos de Luli",
     "ubicacion": "Repostería Artesanal, Pacasmayo",
@@ -63,7 +63,7 @@ if os.path.exists(img_folder):
                     </div>
                 </div>'''
 
-# Cargamos tu video local y el logo
+# Video local y logo
 video_path = f"{BASE_URL}/static/video/portada.mp4"
 logo_path = f"{BASE_URL}/logo.png"
 
@@ -72,8 +72,8 @@ def cargar_web():
     with open("templates/index.html", "r", encoding="utf-8") as f:
         html = f.read()
         
-        serv_html = "".join([f'<div class="col-md-4 col-6" data-aos="fade-up"><div class="service-card shadow-sm"><i class="fas {s["icono"]} mb-3" style="color:#d4a373"></i><h6>{s["nombre"]}</h6></div></div>' for s in SERVICIOS])
-        test_html = "".join([f'<div class="col-md-4 mb-4" data-aos="fade-up"><div class="review-box"><div class="stars mb-2" style="color:#d4a373">★★★★★</div><p>"{t["comentario"]}"</p><div class="reviewer-name" style="color:#d4a373">{t["nombre"]}</div></div></div>' for t in TESTIMONIOS])
+        serv_html = "".join([f'<div class="col-md-4 col-6" data-aos="fade-up"><div class="service-card shadow-sm"><i class="fas {s["icono"]} mb-3" style="color:#e62e84"></i><h6>{s["nombre"]}</h6></div></div>' for s in SERVICIOS])
+        test_html = "".join([f'<div class="col-md-4 mb-4" data-aos="fade-up"><div class="review-box"><div class="stars mb-2" style="color:#e62e84">★★★★★</div><p>"{t["comentario"]}"</p><div class="reviewer-name" style="color:#e62e84">{t["nombre"]}</div></div></div>' for t in TESTIMONIOS])
 
         html = html.replace("{{ servicios_items }}", serv_html)
         html = html.replace("{{ testimonios_items }}", test_html)
